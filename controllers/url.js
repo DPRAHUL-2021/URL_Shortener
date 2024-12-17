65229105d9345ce80a9ac60223acf2fae52d9f0c
DPRAHUL <dpranavr2021@gmail.com>
2024-12-17 22:35:08 +0530

    Routes updated , MongoDB Connected , URL Shortener is working

--- controllers/url.js
+++ controllers/url.js
@@ -1,23 +1,34 @@
 const express = require("express");
-import { nanoid } from 'nanoid'
-const URLSchema = require("../models/url");
-
+// shortid is an npm package which is used to generate short-urls
+const shortid =  require("shortid");
+const URL = require("../models/url");
 
 async function generateShortURL(req,res){
     const body = req.body;
     if(!body.url){
         return res.status(400).json({error : "Url Is Required"});
     }
-    const shortId = nanoid(8);
+    const shortId = shortid.generate();
     await URL.create({
-        shortId:shortId,
+        shortID:shortId,
         redirectURL:body.url,
         clickHistory : [],
     });
-
     return res.json({id : shortId});
 }
 
+
+async function getAnalytics(req,res){
+    const shortId = req.params.shortID;
+    const result = await URL.findOne({shortID:shortId});
+    res.json({
+        totalClicks : result.clickHistory.length,
+        analytics:result.clickHistory,
+    });
+}
+
+
 module.exports = {
-    generateShortURL
+    generateShortURL,
+    getAnalytics,
 }
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
-const PORT = 8001;
+const PORT = 8000;
+const URL = require("./models/url")
+const {connectMongoDB} = require("./connect");
 
 const URLRoute = require("./routes/url");
+connectMongoDB("mongodb://localhost:27017/short-url").then(()=>{
+    console.log("Connection Established With MongoDB")
+})
+
+// Middleware so that the body is parsed in PostMan
+app.use(express.json());
 
 app.use("/url",URLRoute);
+
+
+app.get("/:shortID", async(req,res)=>{
+    const shortId = req.params.shortID;
+    const entry = await URL.findOneAndUpdate({
+        shortID:shortId
+    },{
+        $push:{clickHistory : {
+            timestamp:Date.now(),
+        },},
+    });
+    res.redirect(entry.redirectURL);
+})
 
 app.listen(PORT,()=>{
     console.log(`Server Started At PORT:${PORT}`);
